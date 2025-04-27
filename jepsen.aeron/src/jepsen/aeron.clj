60d5f05763b3dd9441e875175aeb6db91cde5020
Helen Ilkiu <hilkiu2@localhost>
2025-04-26 20:48:57 -0600

    progress

--- jepsen.aeron/src/jepsen/aeron.clj
+++ jepsen.aeron/src/jepsen/aeron.clj
@@ -21,32 +21,23 @@
           :pure-generators true
           :db (db/db "v1.47.4")
           :leave-db-running? false
-          :concurrency 10 
+          :concurrency 40
           :client (client/->Client nil)
-          :nemesis (nemesis/partition-random-halves) ;; Used to soley test network partitions
-          ;; :nemesis (aeron-nemesis/slow-wrapper (nemesis/partition-random-halves) "enp6s7" 0.5) ;; Used to test network partitions under a 0.5 sec delay
+          ;; :nemesis (nemesis/partition-random-halves) ;; Used to soley test network partitions
+          :nemesis (aeron-nemesis/slow-wrapper (nemesis/partition-random-halves) "enp6s7" 0.5) ;; Used to test network partitions under a 0.5 sec delay
           ;; :nemesis (aeron-nemesis/startstop 1)
           :generator (gen/phases
-                        (->> (gen/mix [
-                                (fn [_ _]
-                                  {:type :invoke
-                                  :f    :bid
-                                  :value {:id    (+ 1 (rand-int 10))       ;1-10
-                                          :price (+ 1 (rand-int 10000000))}})  ;1-10,000,000
-
-                                (fn [_ _]
-                                  {:type :invoke
-                                  :f    :status
-                                  :value nil})])
-                            (gen/clients)
-                            (gen/nemesis
-                              (->> (cycle [{:type :info, :f :start}
-                                           (gen/sleep 5)
-                                           {:type :info, :f :stop}
-                                           (gen/sleep 5)])
-                                   (take 20)))
-                            (gen/stagger 1/500)
-                            (gen/time-limit (:time-limit opts))))
+                        (->>  (gen/mix [ client/bid client/status])
+                              (gen/clients)
+                              (gen/nemesis
+                                (->> (cycle [{:type :info, :f :start}
+                                            (gen/sleep 5)
+                                            {:type :info, :f :stop}
+                                            (gen/sleep 5)])
+                                    ;; (take 20)
+                                    ))
+                              (gen/stagger 1/50)
+                              (gen/time-limit (:time-limit opts))))
           :checker (checker/compose 
                     { :linear (checker/linearizable {:model (auction-model/auction-model)})
                       :perf      (checker/perf)
--- jepsen.aeron/src/jepsen/aeron/client.clj
+++ jepsen.aeron/src/jepsen/aeron/client.clj
@@ -4,10 +4,26 @@
               [jepsen.client :as client]
               [cheshire.core :as json]))
 
+(def winning-price (atom 0))
+
+(defn bid []
+  (fn [_ _]
+    (let [current-winning-price @winning-price]
+      {:type :invoke
+       :f :bid
+       :value {:id (+ 1 (rand-int 50))
+               :price (+ current-winning-price (rand-int 10))}})))
+
+(defn status []
+  (fn [_ _]
+    {:type :invoke
+     :f    :status
+     :value nil}))
+
 (defrecord Client [conn]
   client/Client
   (open! [this test node]
-    (assoc this :conn (str "http://" node ":8081")))
+    (assoc this :conn (str "http://" node ":8081") :winning-price 0))
 
   (setup! [this test]
     ;; (info "Waiting 30 seconds to allow HTTP server to bind to port 8081...")
@@ -26,32 +42,30 @@
   (invoke! [this test op]
     (let [url (:conn this)]
       (case (:f op)
-        :bid
-        (try
-          (let [{:keys [id price succeeded]} (:value op)
-                full-url (str url "/bid")
-                params {:customerId id :price price}]
-            ;; (info "Placing bid on auction:" id "with price:" price "at" full-url)
-            (let [response (http/post full-url {:form-params params
-                                                :accept :json
-                                                :as :text})
-                  ;; _ (info "RAW JSON:" (:body response))     
-                  body (try
-                        (json/parse-string (:body response) true)
-                        (catch Exception e
-                          (warn "Failed to parse bid response JSON: " (.getMessage e))
-                          {:parse-error (.getMessage e)}))
-                  bid-succeed (:bidSucceed body)]
-              ;; (info "Sent bid request with" params)
-              ;; (info "Response body parsed as:" body)
-              (assoc op :type :ok
-                    :value {:id id :price price :succeeded bid-succeed})))
-          (catch Exception e
-            (let [ex (ex-data e)
-                  raw-body (:body ex)]
-              (warn "Failed to place bid: " (.getMessage e))
-              (when raw-body (warn "Raw error body: " raw-body))
-              (assoc op :type :fail :error (or raw-body (.getMessage e))))))
+      :bid
+      (try
+        (let [{:keys [id price]} (:value op)
+              full-url (str url "/bid")
+              params {:customerId id :price price}]
+          (let [response (http/post full-url {:form-params params :accept :json :as :text})
+                body (try
+                      (json/parse-string (:body response) true)
+                      (catch Exception e
+                        (warn "Failed to parse bid response JSON: " (.getMessage e))
+                        {:parse-error (.getMessage e)}))
+                bid-succeed (:bidSucceed body)]
+            (assoc op :type :ok
+                  :value {:id id :price price :succeeded bid-succeed})))
+        (catch clojure.lang.ExceptionInfo e
+          (let [data (ex-data e)
+                status (:status data)]
+            (if (= status 504)
+              (assoc op :type :fail :error :timeout)
+              (do (warn "Failed to place bid:" (.getMessage e))
+                  (assoc op :type :fail :error :unknown)))))
+        (catch Exception e
+          (warn "Unknown failure placing bid:" (.getMessage e))
+          (assoc op :type :fail :error :unknown)))
 
         :status
         (try
@@ -62,22 +76,20 @@
                       (catch Exception e
                         (warn "Failed to parse status JSON: " (.getMessage e))
                         {:parse-error (.getMessage e)}))
-            {:keys [winningCustomerId winningPrice]} body]
-            ;; (info "Received status:" body)
+                {:keys [winningCustomerId winningPrice]} body]
+            (reset! winning-price winningPrice)
             (assoc op :type :ok :value {:id winningCustomerId :price winningPrice}))
+          (catch clojure.lang.ExceptionInfo e
+            (let [data (ex-data e)
+                  status (:status data)]
+              (if (= status 504)
+                (assoc op :type :fail :error :timeout)
+                (do (warn "Failed to place bid:" (.getMessage e))
+                    (assoc op :type :fail :error :unknown)))))
           (catch Exception e
-            (let [ex (ex-data e)
-                  raw-body (:body ex)]
-              (warn "Failed to get status:" (.getMessage e))
-              (when raw-body (warn "Raw error body:" raw-body))
-              (assoc op :type :fail :error (or raw-body (.getMessage e)))))))))
+            (warn "Unknown failure placing bid:" (.getMessage e))
+            (assoc op :type :fail :error :unknown))))))
 
-
-  (teardown! [this test])
-    ;; (ignore-errors (c/su (c/exec :pkill :-f "AuctionHttpServer")))
-  
+  (teardown! [this test])  
 
   (close! [this test]))
-    ;; (info "Closing HTTP server")
-    ;;   (ignore-errors (c/su (c/exec :pkill :-f "AuctionHttpServer")))
-  
