1b442a35af907424fb3e5ba14fadb03fee8c690c
Helen Ilkiu <hilkiu2@localhost>
2025-05-05 17:17:27 -0600

    abandon

--- jepsen.aeron/src/jepsen/aeron.clj
+++ jepsen.aeron/src/jepsen/aeron.clj
@@ -30,9 +30,9 @@
           :pure-generators true
           :db (db/db "v1.47.4" hostname)
           :leave-db-running? false
-          :concurrency 20
-          :client (client/->Client nil)
-          :nemesis (aeron-nemesis/partition-random-halves) ;; Used to soley test network partitions
+          :concurrency 1
+          :client (client/->Client nil nil)
+          ;; :nemesis (aeron-nemesis/partition-random-halves) ;; Used to soley test network partitions
           ;; :nemesis (aeron-nemesis/network-delay :all 0.5) ;; Used to test 0.5 sec network delay for all nodes
           ;; :nemesis (aeron-nemesis/network-delay :random 0.5) ;; Used to test 0.5 sec network delay for a single random node
           ;; :nemesis (aeron-nemesis/random-packet-loss :all 10) ;; Used to test 10% packet loss for all nodes
@@ -41,7 +41,7 @@
           :generator (gen/phases
                         ;; Phase 1: Warmup (no nemesis)
                         (->> (independent/concurrent-generator
-                              2
+                              1
                               (range 0 10)
                               (fn [item-id]
                                 (->> (gen/mix [(repeat 8 (client/bid item-id))
@@ -51,17 +51,17 @@
 
                         ;; Phase 2: Real test with nemesis
                         (->> (independent/concurrent-generator
-                              2
+                              1
                               (range 0 10)
                               (fn [item-id]
                                 (->> (gen/mix [(repeat 8 (client/bid item-id))
                                                 (repeat 2 (client/item item-id))])
                                       (gen/stagger 1/500))))
-                            (gen/nemesis
-                              (cycle [(gen/sleep 5)
-                                      {:type :info, :f :start}
-                                      (gen/sleep 5)
-                                      {:type :info, :f :stop}]))
+                            ;; (gen/nemesis
+                            ;;   (cycle [(gen/sleep 5)
+                            ;;           {:type :info, :f :start}
+                            ;;           (gen/sleep 5)
+                            ;;           {:type :info, :f :stop}]))
                             (gen/time-limit (:time-limit opts))))                             
           :checker (checker/compose
                     { :perf  (checker/perf)
--- jepsen.aeron/src/jepsen/aeron/client.clj
+++ jepsen.aeron/src/jepsen/aeron/client.clj
@@ -7,6 +7,8 @@
 
 (def winning-prices (atom {}))
 
+(def pending-responses (atom {})) ;; corr-id -> op
+
 (defn bid [item-id]
   (fn [_ _]
     (let [
@@ -25,58 +27,74 @@
   (fn [_ _]
     {:type :invoke
      :f    :item
-    ;;  :value [item-id]
      }))
 
-(defrecord Client [conn]
+(defn response-handler [test]
+  (fn [req]
+    (let [{:keys [corr-id item-id price succeeded]}
+          (json/read-str (slurp (:body req)) :key-fn keyword)
+          op (get @pending-responses corr-id)]
+      (when op
+        (swap! pending-responses dissoc corr-id)
+        (client/invoke-complete!
+          test
+          (assoc op
+            :type :ok
+            :value (independent/tuple item-id {:id item-id :price price :succeeded succeeded}))))
+      {:status 200 :body "ok"})))
+
+(defn start-response-server! [test]
+  (run-jetty (response-handler test) {:port 8082 :join? false :max-threads 4096}))
+
+(defrecord Client [conn-invoke conn-response]
   client/Client
   (open! [this test node]
-    (assoc this :conn (str "http://" node ":8081") :winning-price 0))
+    (assoc this :conn-response (str "http://" node ":8082"))
+    (assoc this :conn-invoke (str "http://" node ":8081") :winning-price 0))
 
   (setup! [this test]
     ;; (info "Waiting 30 seconds to allow HTTP server to bind to port 8081...")
     (Thread/sleep 30000)
 
+    (start-response-server! test)
+
     (let [url (str "http://localhost:8081/health")
           response (http/get url {:throw-exceptions false})]
       (info "HTTP server health check response " (:body response)))
     
-    (assoc this :conn "http://localhost:8081"))
+    (assoc this :conn-invoke "http://localhost:8081"))
 
   (invoke! [this test op]
-    (let [url (:conn this)]
+    (let [url (:conn-invoke this)]
       (case (:f op)
       :bid
       (try
         (let [[id price] (:value op)
               full-url (str url "/bid")
-              params {:itemId id :price price}]
+              corrId (rand-int Integer/MAX_VALUE)
+              params {:itemId id :price price :corrId corrId}]
           (let [response (http/post full-url {:form-params params :accept :json :as :text})
-                body (try
-                      (json/parse-string (:body response) true)
-                      (catch Exception e
-                        (warn "Failed to parse bid response JSON: " (.getMessage e))
-                        {:parse-error (.getMessage e)}))
-                {:keys [itemId price success]} body]
-            (swap! winning-prices assoc itemId price)
-            (assoc op :type :ok :value (independent/tuple itemId {:id itemId :price price :succeeded success}))))
+                  body (try
+                        (json/parse-string (:body response) true)
+                        (catch Exception e
+                          (warn "Failed to parse bid response JSON: " (.getMessage e))
+                          {:parse-error (.getMessage e)}))
+                  {:keys [status]} body] 
+            (if (= status "ACK")
+              (do
+                (swap! pending-responses assoc corr-id op) ;; only add if we saw ACK
+                (assoc op :type :invoke)) ;; Jepsen sees this as an in-flight op
+              (do
+                (warn "No ACK received; skipping op tracking")
+                (assoc op :type :fail :error :no-ack)))))
         (catch Exception e
-          (warn "Exception caught placing bid: " (.getMessage e))
-          (let [data (ex-data e)]
-            (cond
-              (= (:status data) 504)
-              (assoc op :type :fail :error :timeout)
-
-              (some? data)
-              (assoc op :type :fail :error :unknown)
-
-              :else
-              (assoc op :type :fail :error :unknown)))))
-
+          (warn "Failed to send bid: " (.getMessage e))
+          (assoc op :type :fail :error :client-error)))
 
         :item
         (try
           (let [[id] (:value op)
+                corr-id (rand-int Integer/MAX_VALUE)
                 full-url (str url "/item")
                 params {:itemId id}]
             (let [response (http/get full-url {:query-params params :accept :json :as :text})
@@ -85,21 +103,17 @@
                         (catch Exception e
                           (warn "Failed to parse item response JSON: " (.getMessage e))
                           {:parse-error (.getMessage e)}))
-                  {:keys [itemId price success]} body]
-              (swap! winning-prices assoc itemId price)
-              (assoc op :type :ok :value (independent/tuple itemId {:id itemId :price price :succeeded success}))))
+                  {:keys [status]} body]
+                (if (= status "ACK")
+                  (do
+                    (swap! pending-responses assoc corr-id op) ;; only add if we saw ACK
+                    (assoc op :type :invoke)) ;; Jepsen sees this as an in-flight op
+                  (do
+                    (warn "No ACK received; skipping op tracking")
+                    (assoc op :type :fail :error :no-ack)))))
           (catch Exception e
-            (warn "Exception caught querying item: " (.getMessage e))
-            (let [data (ex-data e)]
-              (cond
-                (= (:status data) 504)
-                (assoc op :type :fail :error :timeout)
-
-                (some? data)
-                (assoc op :type :fail :error :unknown)
-
-                :else
-                (assoc op :type :fail :error :unknown))))))))
+            (warn "Failed to send item: " (.getMessage e))
+            (assoc op :type :fail :error :client-error))))))
 
 
   (teardown! [this test])  
